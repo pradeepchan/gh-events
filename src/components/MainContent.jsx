e5a92418d5a285559b4ade3408aabd10951a481c
pradeepchan <pradeep.ppkumar@gmail.com>
2024-04-13 20:20:13 -0400

    updated file [skip ci]

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -9,7 +9,6 @@ function MainContent() {
     setHelpVisible((isVisible) => !isVisible);
   }
 
-  // testing
   return (
     <main>
       <button onClick={toggleHelp}>{helpVisible ? 'Hide' : 'Show'} Help</button>
